c3ccef5729c375269c94290f5c11d2429b1d46d8
Mostafa Farrag <moah.farag@gmail.com>
2023-09-24 21:28:11 +0200

    add validation method

--- cleopatra/colors.py
+++ cleopatra/colors.py
@@ -1,5 +1,32 @@
+from typing import List, Union
+import re
+
+# from PIL import ImageColor
+HEX_COLOR_REGEX = re.compile(r"^#(?:[0-9a-fA-F]{3}){1,2}$")
+
+
 class Colors:
     """Colors class for Cleopatra."""
 
-    def __init__(self, hex_color: str):
+    def __init__(self, hex_color: Union[List[str], str]):
+        # convert the hex color to a list if it is a string
+        if isinstance(hex_color, str):
+            hex_color = [hex_color]
+
         self.hex_color = hex_color
+
+    def is_valid_hex(self) -> List[bool]:
+        """is_valid_hex.
+
+            is_valid_hex
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        """
+        return [
+            True if HEX_COLOR_REGEX.search(col) else False for col in self.hex_color
+        ]
--- tests/test_colors.py
+++ tests/test_colors.py
@@ -2,17 +2,15 @@ from cleopatra.colors import Colors
 
 
 def test_create_colors_object():
-    """test_create_colors_object.
-
-        test_create_colors_object
-
-    Parameters
-    ----------
-
-    Returns
-    -------
-
-    """
+    """test_create_colors_object."""
     hex_number = "ff0000"
     color = Colors(hex_number)
-    assert color.hex_color == hex_number
+    assert color.hex_color == [hex_number]
+
+
+def test_is_valid():
+    """test_create_colors_object."""
+    hex_number = ["ff0000", "#23a9dd"]
+    color = Colors(hex_number)
+    valid = color.is_valid_hex()
+    assert valid == [False, True]
