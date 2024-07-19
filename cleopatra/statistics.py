c539d149601483e042265eb4367139eae32b3ee8
Mostafa Farrag <moah.farag@gmail.com>
2024-07-19 23:19:16 +0200

    add docstring

--- cleopatra/statistics.py
+++ cleopatra/statistics.py
@@ -54,14 +54,29 @@ class Statistic:
         ----------
         **kwargs: [dict]
             keys:
-                bins: [int]
+                bins: int, Default is 15.
                     number of bins.
-                color: [str]
-                    color of the bins
-                alpha: [float]
-                    degree of transparency.
-                rwidth: [float]
+                color: List[str], default is ["#0504aa"]
+                    color of the bins, the number of colors should be equal to the number of samples (columns of the
+                    given array).
+                alpha: float, default is 0.7
+                     degree of transparency.
+                rwidth: float, default is 0.85
                     width of the bins.
+                grid_alpha:
+                    alpha of the grid.
+                xlabel: str
+                    x-axis label.
+                ylabel: str
+                    y-axis label.
+                xlabel_font_size: int
+                    x-axis label font size.
+                ylabel_font_size: int
+                    y-axis label font size.
+                xtick_font_size: int
+                    x-axis tick font size.
+                 ytick_font_size: int
+                    y-axis tick font size.
 
         Raises
         ------
@@ -78,7 +93,44 @@ class Statistic:
                 >>> x = 4 + np.random.normal(0, 1.5, 200)
                 >>> stat_plot = Statistic(x)
                 >>> fig, ax, hist = stat_plot.histogram()
+                >>> print(hist) # doctest: +SKIP
+                {'n': [array([ 2.,  4.,  3., 10., 11., 20., 30., 27., 31., 25., 17.,  8.,  5.,
+                        6.,  1.])], 'bins': [array([0.34774335, 0.8440597 , 1.34037605, 1.8366924 , 2.33300874,
+                       2.82932509, 3.32564144, 3.82195778, 4.31827413, 4.81459048,
+                       5.31090682, 5.80722317, 6.30353952, 6.79985587, 7.29617221,
+                       7.79248856])], 'patches': [<BarContainer object of 15 artists>]}
 
+        .. image:: /_images/one-histogram.png
+            :alt: Example Image
+            :align: center
+
+        - 2D data.
+
+            - First genearte some random data and plot the histogram.
+
+                >>> np.random.seed(1)
+                >>> x = 4 + np.random.normal(0, 1.5, (200, 3))
+                >>> stat_plot = Statistic(x, color=["red", "green", "blue"], alpha=0.4, rwidth=0.8)
+                >>> fig, ax, hist = stat_plot.histogram()
+                >>> print(hist) # doctest: +SKIP
+                {'n': [array([ 1.,  2.,  4., 10., 13., 19., 20., 32., 27., 23., 24., 11.,  5.,
+                        5.,  4.]), array([ 3.,  4.,  9., 12., 20., 41., 29., 32., 25., 14.,  9.,  1.,  0.,
+                        0.,  1.]), array([ 3.,  4.,  6.,  7., 25., 26., 31., 24., 30., 19., 11.,  9.,  4.,
+                        0.,  1.])], 'bins': [array([-0.1896275 ,  0.33461786,  0.85886323,  1.38310859,  1.90735396,
+                        2.43159932,  2.95584469,  3.48009005,  4.00433542,  4.52858078,
+                        5.05282615,  5.57707151,  6.10131688,  6.62556224,  7.14980761,
+                        7.67405297]), array([-0.1738017 ,  0.50031202,  1.17442573,  1.84853945,  2.52265317,
+                        3.19676688,  3.8708806 ,  4.54499432,  5.21910804,  5.89322175,
+                        6.56733547,  7.24144919,  7.9155629 ,  8.58967662,  9.26379034,
+                        9.93790406]), array([0.24033902, 0.7940688 , 1.34779857, 1.90152835, 2.45525813,
+                       3.0089879 , 3.56271768, 4.11644746, 4.67017723, 5.22390701,
+                       5.77763679, 6.33136656, 6.88509634, 7.43882612, 7.99255589,
+                       8.54628567])], 'patches': [<BarContainer object of 15 artists>,
+                       <BarContainer object of 15 artists>, <BarContainer object of 15 artists>]}
+
+        .. image:: /_images/three-histogram.png
+            :alt: Example Image
+            :align: center
         """
         for key, val in kwargs.items():
             if key not in self.default_options.keys():
