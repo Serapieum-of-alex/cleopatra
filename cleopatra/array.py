068dbaaadda9b2047823df891f0b2654b16b02bb
Mostafa Farrag <moah.farag@gmail.com>
2024-07-20 16:15:49 +0200

    add example to Array class

--- cleopatra/array.py
+++ cleopatra/array.py
@@ -125,7 +125,7 @@ class Array:
             >>> import numpy as np
             >>> arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
             >>> array = Array(arr)
-            >>> array.plot()
+            >>> fig, ax = array.plot()
 
         .. image:: /_images/image-plot.png
             :alt: Example Image
