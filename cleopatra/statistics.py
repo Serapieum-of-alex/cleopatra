9790107d35cc5ebf2d4d80792d579b391929f67a
Mostafa Farrag <moah.farag@gmail.com>
2024-07-20 17:23:38 +0200

    correct indundations

--- cleopatra/statistics.py
+++ cleopatra/statistics.py
@@ -26,30 +26,29 @@ The class has the following key features:
 Here's an example of how to use the `Statistic` class:
 
 ```python
+import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
+from cleopatra.statistics import Statistic
 
-    import numpy as np
-    import matplotlib
-    matplotlib.use("TkAgg")
-    from cleopatra.statistics import Statistic
+# Create some random 1D data
+np.random.seed(1)
+data_1d = 4 + np.random.normal(0, 1.5, 200)
 
-    # Create some random 1D data
-    np.random.seed(1)
-    data_1d = 4 + np.random.normal(0, 1.5, 200)
+# Create a Statistic object with the 1D data
+stat_plot_1d = Statistic(data_1d)
 
-    # Create a Statistic object with the 1D data
-    stat_plot_1d = Statistic(data_1d)
+# Generate a histogram plot for the 1D data
+fig_1d, ax_1d, hist_1d = stat_plot_1d.histogram()
 
-    # Generate a histogram plot for the 1D data
-    fig_1d, ax_1d, hist_1d = stat_plot_1d.histogram()
+# Create some random 2D data
+data_2d = 4 + np.random.normal(0, 1.5, (200, 3))
 
-    # Create some random 2D data
-    data_2d = 4 + np.random.normal(0, 1.5, (200, 3))
+# Create a Statistic object with the 2D data
+stat_plot_2d = Statistic(data_2d, color=["red", "green", "blue"], alpha=0.4, rwidth=0.8)
 
-    # Create a Statistic object with the 2D data
-    stat_plot_2d = Statistic(data_2d, color=["red", "green", "blue"], alpha=0.4, rwidth=0.8)
-
-    # Generate a histogram plot for the 2D data
-    fig_2d, ax_2d, hist_2d = stat_plot_2d.histogram()
+# Generate a histogram plot for the 2D data
+fig_2d, ax_2d, hist_2d = stat_plot_2d.histogram()
 """
 
 from typing import Union, List, Dict
