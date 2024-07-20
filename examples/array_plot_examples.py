06d9f911a0e9cd27f708166a76bbacb4db9010ed
Mostafa Farrag <moah.farag@gmail.com>
2024-07-20 21:34:34 +0200

    adjust scripts

--- examples/array_plot_examples.py
+++ examples/array_plot_examples.py
@@ -6,13 +6,14 @@ import numpy as np
 from cleopatra.array import Array
 
 # from matplotlib.transforms import blended_transform_factory
-#%%
+# %%
 arr = np.load("tests/data/s2a.npy")
 arr = np.moveaxis(arr, 0, -1)
 arr2 = arr[:, :, [3, 2, 1]]
 rgb = np.clip(arr2 / 10000, 0, 1)
 plt.imshow(rgb)
-#%%
+plt.show()
+# %%
 arr = np.load("tests/data/arr.npy")
 exclude_value = arr[0, 0]
 cmap = "terrain"
@@ -20,16 +21,16 @@ arr2 = np.load("tests/data/DEM5km_Rhine_burned_fill.npy")
 exculde_value2 = arr2[0, 0]
 color_scale = [1, 2, 3, 4, 5]
 ticks_spacing = 500
-#%%
+# %%
 array = Array(arr, exclude_value=[exclude_value])
 fig, ax = array.plot(title="Flow Accumulation")
-#%% test_plot_array_color_scale_1
+# %% test_plot_array_color_scale_1
 fig, ax = array.plot(
     color_scale=color_scale[0],
     cmap=cmap,
     ticks_spacing=ticks_spacing,
 )
-#%% test_plot_array_color_scale_2
+# %% test_plot_array_color_scale_2
 color_scale_2_gamma = 0.5
 fig, ax = array.plot(
     color_scale=color_scale[1],
@@ -37,7 +38,7 @@ fig, ax = array.plot(
     gamma=color_scale_2_gamma,
     ticks_spacing=ticks_spacing,
 )
-#%% test_plot_array_color_scale_3
+# %% test_plot_array_color_scale_3
 ticks_spacing = 5
 color_scale_3_linscale = 0.001
 color_scale_3_linthresh = 0.0001
@@ -48,14 +49,14 @@ fig, ax = array.plot(
     cmap=cmap,
     ticks_spacing=ticks_spacing,
 )
-#%% test_plot_array_color_scale_4
+# %% test_plot_array_color_scale_4
 ticks_spacing = 10
 fig, ax = array.plot(
     color_scale=color_scale[3],
     cmap=cmap,
     ticks_spacing=ticks_spacing,
 )
-#%%
+# %%
 # bounds = [-559, 0, 440, 940, 1440, 1940, 2440, 2940, 3500]
 # bounds = [0,  440,  940, 1440, 1940, 2440, 2940, 3500]
 bounds = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
@@ -67,7 +68,7 @@ fig, ax = array.plot(
     ticks_spacing=ticks_spacing,
 )
 
-#%% test_plot_array_color_scale_5
+# %% test_plot_array_color_scale_5
 midpoint = 20
 
 fig, ax = array.plot(
@@ -87,7 +88,7 @@ fig, ax = array.plot(
     background_color_threshold=background_color_threshold,
     ticks_spacing=ticks_spacing,
 )
-#%%
+# %%
 coello_data = np.load("tests/data/coello.npy")
 exclude_value = arr[0, 0]
 animate_time_list = list(range(1, 11))
@@ -101,7 +102,7 @@ import sys
 
 # path = Path(sys.executable)
 # f"{path.parent}/{}"
-#%%
+# %%
 
 # amin.save('examples/animation.gif', fps=2)
 video_format = "mp4"
