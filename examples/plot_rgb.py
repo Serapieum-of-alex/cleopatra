cc6e69b4c8f1ba07d7c78083fcb6f9868b92b780
Mostafa Farrag <moah.farag@gmail.com>
2024-07-28 20:53:30 +0200

    update example script

--- examples/plot_rgb.py
+++ examples/plot_rgb.py
@@ -1,14 +1,15 @@
 import matplotlib
 
 matplotlib.use("TkAgg")
+
 import numpy as np
 from cleopatra.array_glyph import ArrayGlyph
 
 sentinel_2 = np.load("tests/data/s2a.npy")
-
+extent = [34.626902783650785, 34.654007151597256, 31.82337186561403, 31.8504762335605]
 # %% process the channels by excluding the top and lowest 1% of the values.
-array = ArrayGlyph(sentinel_2, rgb=[3, 2, 1], percentile=1)
-array.plot()
+array = ArrayGlyph(sentinel_2, rgb=[3, 2, 1], percentile=1, extent=extent)
+fig, ax = array.plot()
 # %% process the channels by the surface reflectance of the sentinel data,
 array = ArrayGlyph(sentinel_2, rgb=[3, 2, 1], surface_reflectance=10000)
 array.plot()
