2c96485076ea7cb863792cd02129bb1a88fbe790
Mostafa Farrag <moah.farag@gmail.com>
2024-07-28 18:52:28 +0200

    update examples

--- examples/array_plot_examples.py
+++ examples/array_plot_examples.py
@@ -3,38 +3,62 @@ import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.ma as ma
 from cleopatra.array_glyph import ArrayGlyph
 
 # from matplotlib.transforms import blended_transform_factory
 # %%
-arr = np.load("tests/data/s2a.npy")
-arr = np.moveaxis(arr, 0, -1)
-arr2 = arr[:, :, [3, 2, 1]]
-rgb = np.clip(arr2 / 10000, 0, 1)
-plt.imshow(rgb)
-plt.show()
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+mask = np.array([[1, 0, 1], [0, 1, 0], [1, 0, 1]])
+arr = ma.masked_array(arr, mask=mask)
+array = ArrayGlyph(arr)
+# array.plot()
 # %%
-array = ArrayGlyph(arr, rgb=[3, 2, 1], cutoff=[0.3, 0.3, 0.3])
+# arr = np.load("tests/data/s2a.npy")
+# arr = np.moveaxis(arr, 0, -1)
 # %%
 arr = np.load("tests/data/arr.npy")
+arr = np.array([arr, arr])
+# arr = np.moveaxis(arr, 0, -1)
 exclude_value = arr[0, 0]
 cmap = "terrain"
-arr2 = np.load("tests/data/DEM5km_Rhine_burned_fill.npy")
-exculde_value2 = arr2[0, 0]
-color_scale = [1, 2, 3, 4, 5]
-ticks_spacing = 500
+# arr2 = np.load("tests/data/DEM5km_Rhine_burned_fill.npy")
+# exclude_value2 = arr2[0, 0]
+color_scale = ["linear", "power", "lognorm", "boundary", "midpoint"]
+ticks_spacing = 10
+
+
+midpoint = 10
+array = ArrayGlyph(arr, exclude_value=[exclude_value])
+array.plot(
+    color_scale=color_scale[4],
+    midpoint=midpoint,
+    cmap=cmap,
+    ticks_spacing=ticks_spacing,
+)
 # %%
 array = ArrayGlyph(arr, exclude_value=[exclude_value])
-fig, ax = array.plot(title="Flow Accumulation")
-# %% test_plot_array_color_scale_1
 fig, ax = array.plot(
+    cbar_orientation="vertical",
+    cbar_label_rotation=-90,
+    cbar_label_location="center",
+    cbar_length=0.8,
+    cbar_label_size=10,
+    cbar_label="Discharge m3/s",
+    color_scale="linear",
+    cmap="coolwarm_r",
+)
+# %% test_plot_array_color_scale_1
+array = ArrayGlyph(arr, exclude_value=[exclude_value])
+array.plot(
     color_scale=color_scale[0],
     cmap=cmap,
     ticks_spacing=ticks_spacing,
 )
 # %% test_plot_array_color_scale_2
 color_scale_2_gamma = 0.5
-fig, ax = array.plot(
+array = ArrayGlyph(arr, exclude_value=[exclude_value])
+array.plot(
     color_scale=color_scale[1],
     cmap=cmap,
     gamma=color_scale_2_gamma,
@@ -42,9 +66,11 @@ fig, ax = array.plot(
 )
 # %% test_plot_array_color_scale_3
 ticks_spacing = 5
-color_scale_3_linscale = 0.001
+color_scale_3_linscale = 0.1
 color_scale_3_linthresh = 0.0001
-fig, ax = array.plot(
+color_scale_3_linthresh = 0.015
+array = ArrayGlyph(arr, exclude_value=[exclude_value])
+array.plot(
     color_scale=color_scale[2],
     line_scale=color_scale_3_linscale,
     line_threshold=color_scale_3_linthresh,
@@ -53,7 +79,8 @@ fig, ax = array.plot(
 )
 # %% test_plot_array_color_scale_4
 ticks_spacing = 10
-fig, ax = array.plot(
+array = ArrayGlyph(arr, exclude_value=[exclude_value])
+array.plot(
     color_scale=color_scale[3],
     cmap=cmap,
     ticks_spacing=ticks_spacing,
@@ -62,8 +89,8 @@ fig, ax = array.plot(
 # bounds = [-559, 0, 440, 940, 1440, 1940, 2440, 2940, 3500]
 # bounds = [0,  440,  940, 1440, 1940, 2440, 2940, 3500]
 bounds = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
-
-fig, ax = array.plot(
+array = ArrayGlyph(arr, exclude_value=[exclude_value])
+array.plot(
     color_scale=color_scale[3],
     bounds=bounds,
     cmap=cmap,
@@ -71,9 +98,9 @@ fig, ax = array.plot(
 )
 
 # %% test_plot_array_color_scale_5
-midpoint = 20
-
-fig, ax = array.plot(
+midpoint = 10
+array = ArrayGlyph(arr, exclude_value=[exclude_value])
+array.plot(
     color_scale=color_scale[4],
     midpoint=midpoint,
     cmap=cmap,
@@ -83,8 +110,8 @@ fig, ax = array.plot(
 display_cell_value = True
 num_size = 8
 background_color_threshold = None
-
-fig, ax = array.plot(
+array = ArrayGlyph(arr, exclude_value=[exclude_value])
+array.plot(
     display_cell_value=display_cell_value,
     num_size=num_size,
     background_color_threshold=background_color_threshold,
--- examples/plot_rgb.py
+++ examples/plot_rgb.py
@@ -6,5 +6,19 @@ from cleopatra.array_glyph import ArrayGlyph
 
 sentinel_2 = np.load("tests/data/s2a.npy")
 
-array = ArrayGlyph(sentinel_2, rgb=[3, 2, 1], cutoff=[0.3, 0.3, 0.3])
+# %% process the channels by excluding the top and lowest 1% of the values.
+array = ArrayGlyph(sentinel_2, rgb=[3, 2, 1], percentile=1)
+array.plot()
+# %% process the channels by the surface reflectance of the sentinel data,
+array = ArrayGlyph(sentinel_2, rgb=[3, 2, 1], surface_reflectance=10000)
+array.plot()
+# %%
+array = ArrayGlyph(
+    sentinel_2, rgb=[3, 2, 1], surface_reflectance=10000, cutoff=[0.3, 0.3, 0.3]
+)
+array.plot()
+# %%
+sentinel_2 = np.load("tests/data/gaza-20231002.npy")
+
+array = ArrayGlyph(sentinel_2, rgb=[0, 1, 2])
 array.plot()
